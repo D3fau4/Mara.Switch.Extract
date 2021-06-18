5e78c317c7a310cdbae95e97892585f5e8a17bf2
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-06-18 11:00:35 +0200

    Agregar archivos de proyecto.

---
+++ Mara.switch/FSUtils.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using LibHac;
+using LibHac.Common;
+using LibHac.Fs;
+using LibHac.Fs.Fsa;
+using LibHac.FsSystem;
+
+namespace Mara.Switch
+{
+    class FSUtils
+    {
+        public static Result MountFolder(FileSystemClient fs, string path, string mountname)
+        {
+            return fs.Register(mountname.ToU8Span(), new LocalFileSystem(path));
+        }
+
+        public static Result CopyFile(FileSystemClient fs, string srcPath, string dstPath)
+        {
+            U8Span sourcePath = srcPath.ToU8String();
+            U8Span destPath = dstPath.ToU8String();
+            Result rc = fs.OpenFile(out FileHandle sourceHandle, sourcePath, OpenMode.Read);
+            if (rc.IsFailure()) return rc;
+            Console.WriteLine(Path.GetDirectoryName(dstPath).Replace("\\", "/"));
+            fs.EnsureDirectoryExists(Path.GetDirectoryName(dstPath).Replace("\\", "/"));
+            try
+            {
+                rc = fs.GetFileSize(out long oriFileSize, sourceHandle);
+                if (rc.IsFailure()) return rc;
+                rc = fs.CreateOrOverwriteFile(dstPath, oriFileSize);
+                if (rc.IsFailure()) return rc;
+                rc = fs.OpenFile(out FileHandle destHandle, destPath, OpenMode.All);
+                if (rc.IsFailure()) return rc;
+
+                try
+                {
+                    const int maxBufferSize = 1024 * 1024;
+
+                    rc = fs.GetFileSize(out long fileSize, sourceHandle);
+                    if (rc.IsFailure()) return rc;
+
+                    int bufferSize = (int)Math.Min(maxBufferSize, fileSize);
+
+                    byte[] buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+                    try
+                    {
+                        for (long offset = 0; offset < fileSize; offset += bufferSize)
+                        {
+                            int toRead = (int)Math.Min(fileSize - offset, bufferSize);
+                            Span<byte> buf = buffer.AsSpan(0, toRead);
+
+                            rc = fs.ReadFile(out long _, sourceHandle, offset, buf);
+                            if (rc.IsFailure()) return rc;
+
+                            rc = fs.WriteFile(destHandle, offset, buf, WriteOption.None);
+                            if (rc.IsFailure()) return rc;
+                        }
+                    }
+                    finally
+                    {
+                        ArrayPool<byte>.Shared.Return(buffer);
+                    }
+
+                    rc = fs.FlushFile(destHandle);
+                    if (rc.IsFailure()) return rc;
+                }
+                finally
+                {
+                    if (destHandle.IsValid)
+                        fs.CloseFile(destHandle);
+                }
+            }
+            finally
+            {
+                if (sourceHandle.IsValid)
+                    fs.CloseFile(sourceHandle);
+            }
+
+            return Result.Success;
+        }
+    }
+}
---
+++ Mara.switch/Program.cs
@@ -0,0 +1,95 @@
+﻿using LibHac.Fs;
+using System;
+using System.IO;
+
+namespace Mara.Switch
+{
+    class Program
+    {
+        public static HOS horizon;
+        public static PartitionFS NSP;
+        public static GameCard XCI;
+        public static NCA NCAS;
+        static void Main(string[] args)
+        {
+            horizon = new HOS(args[0]);
+            if (args[1].Contains(".xci"))
+            {
+                XCI = new GameCard(horizon, args[1]);
+                try
+                {
+                    var update = new PartitionFS(args[2]);
+                    NCAS = new NCA(horizon, XCI.MountGameCard(horizon), update.MountPFS0(horizon, "update"));
+                }
+                catch (Exception)
+                {
+                    NCAS = new NCA(horizon, XCI.MountGameCard(horizon));
+                }
+            } 
+            else
+            {
+                NSP = new PartitionFS(args[1]);
+                try
+                {
+                    var update = new PartitionFS(args[2]);
+                    NCAS = new NCA(horizon, NSP.MountPFS0(horizon, "base"), update.MountPFS0(horizon, "update"));
+                }
+                catch (Exception)
+                {
+                    NCAS = new NCA(horizon, NSP.MountPFS0(horizon, "base"));
+                }
+            }
+            NCAS.MountProgram(horizon, args[args.Length - 1]);
+            bool end = false;
+            do
+            {
+                Console.WriteLine("Escoge una opción: \n1) Extraer Romfs y Exefs\n2) Extraer solo romfs\n3) Extraer solo exefs\n4) Salir");
+                var option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        FSUtils.MountFolder(horizon.horizon.Fs, "exefs", "OutExefs");
+                        foreach (DirectoryEntryEx entry in horizon.horizon.Fs.EnumerateEntries("exefs:/"))
+                        {
+                            Console.WriteLine(entry.FullPath);
+                            var res = FSUtils.CopyFile(horizon.horizon.Fs, entry.FullPath, entry.FullPath.Replace("exefs:/", "OutExefs:/"));
+                            Console.WriteLine(res);
+                        }
+                        FSUtils.MountFolder(horizon.horizon.Fs, "romfs", "OutRomfs");
+                        foreach (DirectoryEntryEx entry in horizon.horizon.Fs.EnumerateEntries("romfs:/"))
+                        {
+                            Console.WriteLine(entry.FullPath);
+                            var res  = FSUtils.CopyFile(horizon.horizon.Fs, entry.FullPath, entry.FullPath.Replace("romfs:/", "OutRomfs:/"));
+                            Console.WriteLine(res);
+                        }
+                        break;
+                    case "2":
+                        FSUtils.MountFolder(horizon.horizon.Fs, "romfs", "OutRomfs");
+                        foreach (DirectoryEntryEx entry in horizon.horizon.Fs.EnumerateEntries("romfs:/"))
+                        {
+                            Console.WriteLine(entry.FullPath);
+                            var res = FSUtils.CopyFile(horizon.horizon.Fs, entry.FullPath, entry.FullPath.Replace("romfs:/", "OutRomfs:/"));
+                            Console.WriteLine(res);
+                        }
+                        break;
+                    case "3":
+                        FSUtils.MountFolder(horizon.horizon.Fs, "exefs", "OutExefs");
+                        foreach (DirectoryEntryEx entry in horizon.horizon.Fs.EnumerateEntries("exefs:/"))
+                        {
+                            Console.WriteLine(entry.FullPath);
+                            var res = FSUtils.CopyFile(horizon.horizon.Fs, entry.FullPath, entry.FullPath.Replace("exefs:/", "OutExefs:/"));
+                            Console.WriteLine(res);
+                        }
+                        break;
+                    case "4":
+                        end = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+            }
+            while (end != true);
+        }
+    }
+}
